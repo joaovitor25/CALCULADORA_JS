90c1d60161c906ad782faf6bcb3b0a4c3a81897e
joaovitor25 <joaocgus25@outlook.com>
2020-02-11 12:06:45 -0300

    tentando passar o valor do botão para o input texto.

--- app/js/script.js
+++ app/js/script.js
@@ -14,19 +14,35 @@ function hassim() {
 var numeros = [];
 
 function valor_botao(e) {
-    debugger;
+
 
     if (numeros.length > 0) {
-        numeros[numeros.length] = e.value;
+        if(e.value != "+" &&  "-" &&  "/" &&  "*"){
+            numeros[numeros.length] = e.value;}
+       
     } else {
         numeros[0] = e.value;
 
     }
 
-var x = valida_operador();
+    var x = valida_operador();
 }
 
-function valida_operador(e){
-   
-    return "x"
+function valida_operador(e) {
+
+    
+    var posicao1 = parseInt(numeros[0])
+    var posicao2 = parseInt(numeros[1])
+    alert(posicao1 + posicao2)
 }
+function printar_na_tela(){
+    debugger;
+    var tela = numeros;
+    
+    document.getElementById('printa_na_tela').innerHTML = tela;
+    
+    
+    
+}
+
+    
